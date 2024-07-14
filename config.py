e41cacb8a4e22246164e8548af0ac56b76923891
HarrySon <104372010+sonbao0901@users.noreply.github.com>
2024-07-14 16:25:58 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import pyodbc as odbc
 import pandas as pd
 #define server and database
-server = 'DESKTOP-6LM94UI'
+server = ''
 database = 'stock_db'
 #connect to sql server
 conn = odbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';Trusted_Connection=yes;')
